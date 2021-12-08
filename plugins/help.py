0b1d5e14c9bb492b1d75cfe07b948e6845acffb9
gabrierr <82598985+gabrierr@users.noreply.github.com>
2021-12-08 20:48:24 +0700

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -18,7 +18,7 @@ async def _help(client, message):
         text = tr.HELP_MSG[1],
         parse_mode = "markdown",
         disable_notification = True,
-        reply_markup = InlineKeyboardMarkup(map(1)),
+        reply_markup = inline_keyboard_markup(map(1)),
         reply_to_message_id = message.message_id
     )
 
@@ -30,28 +30,28 @@ async def help_answer(c, callback_query):
     message_id = callback_query.message.message_id
     msg = int(callback_query.data.split('+')[1])
     await c.edit_message_text(chat_id = chat_id,    message_id = message_id,
-        text = tr.HELP_MSG[msg],    reply_markup = InlineKeyboardMarkup(map(msg))
+        text = tr.HELP_MSG[msg],    reply_markup = inline_keyboard_markup(map(msg))
     )
 
 
 def map(pos):
     if(pos==1):
         button = [
-            [InlineKeyboardButton(text = '-->', callback_data = "help+2")]
+            [inline_keyboard_button(text = '-->', callback_data = "help+2")]
         ]
     elif(pos==len(tr.HELP_MSG)-1):
 
         button = [
-            [InlineKeyboardButton(text = 'Support Chat', url = "https://www.github.com/cdfxscrq")],
-            [InlineKeyboardButton(text = 'Feature Request', url = "https://t.me/Akshayan1")],
-            [InlineKeyboardButton(text = '<--', callback_data = f"help+{pos-1}")]
+            [inline_keyboard_button(text = 'Support Chat', url = "https://www.github.com/cdfxscrq")],
+            [inline_keyboard_button(text = 'Feature Request', url = "https://t.me/Akshayan1")],
+            [inline_keyboard_button(text = '<--', callback_data = f"help+{pos-1}")]
 
         ]
     else:
         button = [
             [
-                InlineKeyboardButton(text = '<--', callback_data = f"help+{pos-1}"),
-                InlineKeyboardButton(text = '-->', callback_data = f"help+{pos+1}")
+                inline_keyboard_button(text = '<--', callback_data = f"help+{pos-1}"),
+                inline_keyboard_button(text = '-->', callback_data = f"help+{pos+1}")
             ],
         ]
     return button
